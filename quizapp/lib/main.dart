914e57f1955bb19549c70d3f25c3ac4f50254d77
AhsanSeed <ahsansaeed157wb@gmail.com>
2023-04-08 22:42:51 +0500

    Quiz app part2 working
    1) String  ? getQuestion ; This is null saftiy concept
      (Previous code)
    2) there is one functions use
    3) there are expand widget use
    4) there is one row use
    5) are you complete quiz display a dialog box finish.
    6) click true in below green color tick sign
    7) click false in below white color cross sign

--- quizapp/lib/main.dart
+++ quizapp/lib/main.dart
@@ -1,50 +1,29 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+//TODO: Step 2 - Import the rFlutter_Alert package here.
+import 'package:rflutter_alert/rflutter_alert.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+import 'quiz_brain.dart';
 
-  // This widget is the root of your application.
+QuizBrain quizBrain = QuizBrain();
+
+String  ? getQuestion ;
+
+void main() => runApp(Quizzler());
+
+class Quizzler extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Quiz app',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-
   }
 }
