1528bc0fbf6d1c93e0542bcb1242a7002553facd
AhsanSeed <ahsansaeed157wb@gmail.com>
2023-04-09 16:16:43 +0500

    List Widget in Flutter
    1) There are expand widget use
    2) Less space button more space in text
    3) Store question box
    4) There is one row widget use
    5) Icon list widget
    6) Position icon.ac_unit
    7) Create void function ya hum bad main khtam kr dian gay

--- quizapp/lib/main.dart
+++ quizapp/lib/main.dart
@@ -32,50 +32,33 @@ class QuizPage extends StatefulWidget {
   @override
   _QuizPageState createState() => _QuizPageState();
 }
+Alert(
+context: context,
+title: 'Finished!',
+desc: 'You\'ve reached the end of the quiz.',
+).show();
 
-class _QuizPageState extends State<QuizPage> {
-  List<Icon> scoreKeeper = [];
+//TODO Step 4 Part C - reset the questionNumber,
+quizBrain.reset();
 
-  void checkAnswer(bool userPickedAnswer) {
-    bool? correctAnswer = quizBrain.getCorrectAnswer();
+//TODO Step 4 Part D - empty out the scoreKeeper.
+scoreKeeper = [];
+}
 
-    setState(() {
-      //TODO: Step 4 - Use IF/ELSE to check if we've reached the end of the quiz. If so,
-      //On the next line, you can also use if (quizBrain.isFinished()) {}, it does the same thing.
-      if (quizBrain.isFinished() == true) {
-        //TODO Step 4 Part A - show an alert using rFlutter_alert,
-
-        //This is the code for the basic alert from the docs for rFlutter Alert:
-        //Alert(context: context, title: "RFLUTTER", desc: "Flutter is awesome.").show();
-
-        //Modified for our purposes:
-        Alert(
-          context: context,
-          title: 'Finished!',
-          desc: 'You\'ve reached the end of the quiz.',
-        ).show();
-
-        //TODO Step 4 Part C - reset the questionNumber,
-        quizBrain.reset();
-
-        //TODO Step 4 Part D - empty out the scoreKeeper.
-        scoreKeeper = [];
-      }
-
-      //TODO: Step 6 - If we've not reached the end, ELSE do the answer checking steps below 👇
-      else {
-        if (userPickedAnswer == correctAnswer) {
-          scoreKeeper.add(Icon(
-            Icons.check,
-            color: Colors.green,
-          ));
-        } else {
-          scoreKeeper.add(Icon(
-            Icons.close,
-            color: Colors.white,
-          ));
-        }
-        quizBrain.nextQuestion();
-      }
-    });
-  }
+//TODO: Step 6 - If we've not reached the end, ELSE do the answer checking steps below 👇
+else {
+if (userPickedAnswer == correctAnswer) {
+scoreKeeper.add(Icon(
+Icons.check,
+color: Colors.green,
+));
+} else {
+scoreKeeper.add(Icon(
+Icons.close,
+color: Colors.white,
+));
+}
+quizBrain.nextQuestion();
+}
+});
+}
