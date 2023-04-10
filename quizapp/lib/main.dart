df54cd2ff6cc6d2136a720c14492093e9c16d1e3
AhsanSeed <ahsansaeed157wb@gmail.com>
2023-04-10 22:19:59 +0500

    Final Part
    1) Create object view file
    2) Variable name globle
    3) Display on text
    4) Create user
    5) Create correct ans
    6) Icon display
    7) Icon add list
    8) There are folder means clean architutre
    9) Create a question file like as main dart file
    10) Paramiter constrater
    11) Controler can be control the function
    12) Last Reset
    13) List blank

--- quizapp/lib/main.dart
+++ quizapp/lib/main.dart
@@ -1,64 +1,139 @@
 import 'package:flutter/material.dart';
+import 'package:quizapp_sec_b/quiz_brain.dart';
 
-//TODO: Step 2 - Import the rFlutter_Alert package here.
-import 'package:rflutter_alert/rflutter_alert.dart';
+void main() {
+  runApp(MyApp());
+}
 
-import 'quiz_brain.dart';
+QuizBrain obj = QuizBrain();
 
-QuizBrain quizBrain = QuizBrain();
 
-String  ? getQuestion ;
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-void main() => runApp(Quizzler());
-
-class Quizzler extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.grey.shade900,
-        body: SafeArea(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10.0),
-            child: QuizPage(),
-          ),
+    return MaterialApp(home: Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10.0),
+          child: QuizPage(),
         ),
       ),
-    );
+    ),);
   }
 }
 
+
 class QuizPage extends StatefulWidget {
+  const QuizPage({Key? key}) : super(key: key);
+
   @override
-  _QuizPageState createState() => _QuizPageState();
-}
-Alert(
-context: context,
-title: 'Finished!',
-desc: 'You\'ve reached the end of the quiz.',
-).show();
-
-//TODO Step 4 Part C - reset the questionNumber,
-quizBrain.reset();
-
-//TODO Step 4 Part D - empty out the scoreKeeper.
-scoreKeeper = [];
+  State<QuizPage> createState() => _QuizPageState();
 }
 
-//TODO: Step 6 - If we've not reached the end, ELSE do the answer checking steps below 👇
-else {
-if (userPickedAnswer == correctAnswer) {
-scoreKeeper.add(Icon(
-Icons.check,
-color: Colors.green,
-));
-} else {
-scoreKeeper.add(Icon(
-Icons.close,
-color: Colors.white,
-));
-}
-quizBrain.nextQuestion();
-}
-});
+class _QuizPageState extends State<QuizPage> {
+
+  List<Icon> scoreKeeper = [];
+
+  void fun() {
+    scoreKeeper.add(Icon(Icons.ac_unit, color: Colors.white,),);
+  }
+
+  void CheckAnswer(bool userPicker) {
+    setState(() {
+      bool? correctAnswer = obj.getCorrectAnswer();
+      if(obj.isFinished()==true){
+
+        obj.reset();
+        scoreKeeper = [];
+      }
+
+      if (userPicker == correctAnswer) {
+        print("Correct answer");
+        scoreKeeper.add(Icon(Icons.check, color: Colors.white,));
+
+      }
+      else{
+        scoreKeeper.add(Icon(Icons.credit_card_off, color: Colors.white,));
+      }
+
+      obj.nextQuestion();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                //quizBrain.getQuestionText(),
+                //getQuestion!,
+                obj.getQuestionText().toString(),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.amber,
+              ),
+              child: Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                CheckAnswer(true);
+
+                // getQuestion = quizBrain.getQuestionText();
+                // //The user picked true.
+                // checkAnswer(true);
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.green,
+              ),
+              child: Text(
+                'False',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                print("Clcikec False");
+                CheckAnswer(false);
+              },
+            ),
+          ),
+        ),
+        Row(children: scoreKeeper)
+
+      ],);
+  }
 }
