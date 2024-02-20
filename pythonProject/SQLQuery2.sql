bce4c1b959fc905f60eb2a6ee4aba0ddc93511bc
AhsanSeed <ahsansaeed157wb@gmail.com>
2024-02-20 12:05:03 +0500

    upd

---
+++ pythonProject/SQLQuery2.sql
@@ -0,0 +1,18 @@
+create database task
+create table student
+(
+ID int primary key not null,
+FName varchar(12) not null,
+Age varchar(12) not null,
+Phone varchar(23) not null,
+)
+select * from student
+insert into student values (1,'Ahsan',23,03062447627)
+
+CREATE TABLE EstateProperty
+(
+ItemID int,
+Name varchar(255),
+)
+select * from EstateProperty
+insert into EstateProperty values (1,'Ahsan')
